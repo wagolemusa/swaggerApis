98e084ac9f0714f8c65abb19011f699d37c55df3
kisumutalent <homiemusa@gmail.com>
2018-06-01 11:20:56 +0300

    adding envelope

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_restplus import Api, Resource, fields
 app  = Flask(__name__)
 api  = Api(app)
 
-a_languages = api.model('Language', {'language' : fields.String('The language')})
+a_languages = api.model('Language', {'language' : fields.String('The language')}) #, 'id' : fields.Integer('ID')
 
 languages = []
 python = {'language': 'Python', 'id' : 1}
@@ -12,6 +12,8 @@ languages.append(python)
 
 @api.route('/language')
 class Language(Resource):
+
+    @api.marshal_with(a_languages, envelope='the_data')
     def get(self):
         return languages
     
